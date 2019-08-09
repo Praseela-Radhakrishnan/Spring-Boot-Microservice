97f208bf12b9b00f069f910a508694405d1e3f97
Praseela-Radhakrishnan <praseela.org@gmail.com>
2019-08-09 16:33:40 +0530

    Upload sample microservice

---
+++ Sample Microservice SetUp/EdgeService/src/main/java/com/prasee/edgeService/controller/EdgeController.java
@@ -0,0 +1,52 @@
+package com.prasee.edgeService.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.prasee.edgeService.model.Edge;
+
+@RestController
+@RequestMapping("/")
+public class EdgeController {
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private Environment env;
+
+	@RequestMapping("/")
+	public String home() {
+		// This is useful for debugging
+		// When having multiple instance of gallery service running at different ports.
+		// We load balance among them, and display which instance received the request.
+		return "Hello from Edge Service running at port: " + env.getProperty("local.server.port");
+	}
+
+	@RequestMapping("/{id}")
+	public Edge getGallery(@PathVariable final int id) {
+		System.out.println("ID" + id);
+		// create item object
+		Edge edge = new Edge();
+		edge.setId(id);
+
+		// get list of available images
+		List<Object> images = restTemplate.getForObject("http://ItemService/images/", List.class);
+		edge.setImages(images);
+
+		return edge;
+	}
+
+	// -------- Admin Area --------
+	// This method should only be accessed by users with role of 'admin'
+	// We'll add the logic of role based auth later
+	@RequestMapping("/admin")
+	public String homeAdmin() {
+		return "This is the admin area of Item service running at port: " + env.getProperty("local.server.port");
+	}
+}
