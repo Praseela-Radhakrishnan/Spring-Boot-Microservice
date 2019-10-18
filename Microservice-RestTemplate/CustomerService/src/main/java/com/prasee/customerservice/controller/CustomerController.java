254b9aaed2c7dbf6157f57b805e2088f8c044400
PraseelaRadhakrishnan <praseela.org@gmail.com>
2019-10-18 14:51:07 +0530

    Added Swagger configuration

--- Microservice-RestTemplate/CustomerService/src/main/java/com/prasee/customerservice/controller/CustomerController.java
+++ Microservice-RestTemplate/CustomerService/src/main/java/com/prasee/customerservice/controller/CustomerController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,19 +11,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
+import com.prasee.customerservice.feignClient.Product;
+import com.prasee.customerservice.feignClient.ProductServiceClient;
 import com.prasee.customerservice.model.Customer;
 import com.prasee.customerservice.service.CustomerService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(value = "CustomerController", description = "REST Apis related to Customer Entity!!!!")
 @RestController
 public class CustomerController {
 
+	/*
+	 * @Autowired RestTemplate restTemplate;
+	 */
+
 	@Autowired
-	RestTemplate restTemplate;
+	ProductServiceClient productServiceClient;
 
 	@Autowired
 	CustomerService customerService;
+
+	@ApiOperation(value = "Get list of Customers in the System ", response = List.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Suceess|OK"),
+			@ApiResponse(code = 401, message = "not authorized!"), @ApiResponse(code = 403, message = "forbidden!!!"),
+			@ApiResponse(code = 404, message = "not found!!!") })
 
 	@GetMapping("/Customer")
 	public List<Customer> getAllData() {
@@ -33,11 +48,14 @@ public class CustomerController {
 
 	}
 
+	@ApiOperation(value = "Get specific Customer in the System ")
 	@PostMapping("/Customer")
 	public Customer saveData(@RequestBody Customer customer) {
 		Customer data = customerService.save(customer);
 		return data;
 	}
+
+	@ApiOperation(value = "Get specific Customer in the System ")
 
 	@PutMapping("/Customer")
 	public Customer updateById(@RequestBody Customer customer) {
@@ -45,6 +63,8 @@ public class CustomerController {
 		return data;
 
 	}
+
+	@ApiOperation(value = "Get specific Customer in the System ")
 
 	@GetMapping("/Customers/{id}")
 	public Optional<Customer> getById(@PathVariable("id") int id) {
@@ -55,26 +75,35 @@ public class CustomerController {
 
 	}
 
+	@ApiOperation(value = "Get specific Customer in the System ")
+
 	@DeleteMapping("/Customer/{id}")
 	public void deleteById(@PathVariable("id") int id) {
 		customerService.deleteById(id);
 	}
 
+	/*
+	 * @GetMapping("/Customer/getAllProduct") public String getProduct() { String
+	 * url = "http://localhost:8200/Product"; RestTemplate restTemplate = new
+	 * RestTemplate(); System.out.println("DATA: " + url); // return
+	 * restTemplate.getForObject(url, String.class); return
+	 * restTemplate.exchange(url, HttpMethod.GET, null, String.class).getBody(); }
+	 * 
+	 * @GetMapping("/Customer/getAllProduct/{id}") public String
+	 * getProduct(@PathVariable("id") int id) { String url =
+	 * "http://localhost:8200/Products/" + id; RestTemplate restTemplate = new
+	 * RestTemplate(); System.out.println("DATA: " + url); // return
+	 * restTemplate.getForObject(url, String.class); return
+	 * restTemplate.exchange(url, HttpMethod.GET, null, String.class).getBody(); }
+	 */
+
+	@ApiOperation(value = "Get specific Customer in the System ")
+
 	@GetMapping("/Customer/getAllProduct")
-	public String getProduct() {
-		String url = "http://localhost:8200/Product";
-		// RestTemplate restTemplate = new RestTemplate();
-		System.out.println("DATA: " + url);
-		// return restTemplate.getForObject(url, String.class);
-		return restTemplate.exchange(url, HttpMethod.GET, null, String.class).getBody();
+	public List<Product> getAllProduct() {
+		List<Product> data = productServiceClient.getAllData();
+		System.out.println("Data " + data);
+		return data;
 	}
 
-	@GetMapping("/Customer/getAllProduct/{id}")
-	public String getProduct(@PathVariable("id") int id) {
-		String url = "http://localhost:8200/Products/" + id;
-		// RestTemplate restTemplate = new RestTemplate();
-		System.out.println("DATA: " + url);
-		// return restTemplate.getForObject(url, String.class);
-		return restTemplate.exchange(url, HttpMethod.GET, null, String.class).getBody();
-	}
 }
